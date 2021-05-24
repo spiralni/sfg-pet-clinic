8ca07a9db9d08113b9930491436893c35c88492a
Cesar Lopez <clopez@logicasoft.io>
2021-05-24 12:10:49 -0600

    step 1

---
+++ sfg-pet-clinic/src/test/java/ni/com/spiralni/sfgpetclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ni.com.spiralni.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
